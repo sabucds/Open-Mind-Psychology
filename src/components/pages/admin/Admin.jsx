808239b95084c76d330972726396f6ea6510986a
Liliana Nóbrega <lilisabrina2009@gmail.com>
2021-11-12 17:28:53 -0400

    listo css y responsivo de vista de admin. falta funcionalidad de botones

--- src/components/pages/admin/Admin.jsx
+++ src/components/pages/admin/Admin.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { bd } from "../../../utils/firebaseConfig";
 import EspecialistaCard from './EspecialistaCard';
 import { useState, useEffect } from "react";
-import EspecialistaList from './EspecialistaList';
+import './Admin.css'
 
 const Admin = () => {
     const [loading, setLoading] = useState(true);
@@ -28,6 +28,7 @@ const Admin = () => {
             
             setEspecialistas(especialistaDocs);
             setLoading(false);
+            console.log("setEspecialistas: ");
             console.log(especialistas);
         }catch (e) {
             setError(e);
@@ -41,25 +42,44 @@ const Admin = () => {
         getEspecialistas();
     }, [flag]); //se llama a la función cuando cambie el valor de flag
         
+    
+    // function handleAccept (especialista) {
+    //     bd.ref("users/"+especialista['id']).update({status: "aceptado"});
+    //     flag++; //se cambia el valor de flag para que en el useEffect se llame a la función de especialistas y se vuelva a cargar el componente
+    // }
+
+    // function handleReject (especialista) {
+    //     bd.ref("users/"+especialista['id']).update({status: "rechazado"});
+    //     flag++; //se cambia el valor de flag para que en el useEffect se llame a la función de especialistas y se vuelva a cargar el componente
+    // }
 
 
     return (
         <section className="admin">
-            <div className="titulo">¡Bienvenido administrador!</div>    
-            <p>Se le presentarán los candidatos postulados, considere su decisión:</p>
-            <hr />
+            <div className="titulo">¡Bienvenido administrador!</div>
             <div className="containerEspecialistasAdmin">
+                <p className="introAdmin">Se le presentarán los candidatos postulados, considere su decisión:</p>
+                <hr />
                 { 
                 //si está cargando, muestra "Cargando..."; si no: si hay un error muestra el mensaje de error; 
                 //si no: si hay especialistas que mostrar se muestran y si no, muestra "No hay especialistas nuevos."
-                (loading && !error)? <div className="altText">Cargando...</div> :
-                (error? <div className="altText">Error: {error.message}</div> :
-                (especialistas? <EspecialistaList especialistas={especialistas} /> :
-                <div className="altText">No hay especialistas nuevos.</div>))
-                }
+                (loading && !error) ? <div className="altText">Cargando...</div> :
+                    (error) ? <div className="altText">Error: {error.message}</div> :
+                    (Object.entries(especialistas).length !== 0) ? 
+                    <div className="especialistaList">{Object.keys(especialistas).map((key) => { 
+                        const especialista = especialistas[key]; 
+                        return <EspecialistaCard
+                            key={especialista.id}
+                            especialista={especialista}
+                            />})}</div> : 
+                    <div className="altText">No hay especialistas nuevos.</div>
+                }                        
             </div>   
         </section>
     )
 }
+
+// handleAccept={()=>{handleAccept(especialista)}}
+// handleReject={()=>{handleReject(especialista)}}
 
 export default Admin
--- src/components/pages/admin/EspecialistaCard.jsx
+++ src/components/pages/admin/EspecialistaCard.jsx
@@ -6,22 +6,29 @@ const EspecialistaCard = (props) => {
     return (
         <div className="especialistaCard">
             <div className="espCardTop">
-                <div className="credencialImg"></div>
+                <button type="button" className="credencialButton" title="Click para ver credenciales."></button>
             </div>
-            <div className="nombreEspBox">
-                <div className="nombreEsp">Nombre</div>
-                <p>{props.especialista.name}</p>
+            <div className="espBox">
+                <div className="espBoxCategory">Nombre</div>
+                <p className="espInfo">{props.especialista.name}</p>
             </div>
-            <div className="correoEspBox">
-                <div className="correoEsp">Correo</div>
-                <p>{props.especialista.email}</p>
+            <div className="espBox">
+                <div className="espBoxCategory">Correo</div>
+                <p className="espInfo">{props.especialista.email}</p>
             </div>
             <div className="choiceEspBox">
-                <button type="button" className="aceptarEsp" onClick={props.handleAccept}></button>
-                <button type="button" className="rechazarEsp" onClick={props.handleReject}></button>
+                <button type="button" className="aceptarEsp" title="Click para aceptar al especialista."></button>
+                <button type="button" className="rechazarEsp" title="Click para rechazar al especialista."></button>
             </div>
+
+           
         </div>
     )
 }
+
+// {/* <div className="choiceEspBox">
+// <button type="button" className="aceptarEsp" onClick={props.handleAccept}></button>
+// <button type="button" className="rechazarEsp" onClick={props.handleReject}></button>
+// </div> */}
 
 export default EspecialistaCard;
