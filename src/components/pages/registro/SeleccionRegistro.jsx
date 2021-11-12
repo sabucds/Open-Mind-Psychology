4470b0b3f05755855ae1cd2a1647d9d8fe98468c
Sabrina Correia <sabrina.correia@correo.unimet.edu.ve>
2021-11-11 21:33:02 -0400

    detalles de CSS y seleccion de rol responsive

--- src/components/pages/registro/SeleccionRegistro.jsx
+++ src/components/pages/registro/SeleccionRegistro.jsx
@@ -20,33 +20,38 @@ const SeleccionRegistro = () => {
 
   return (
     <section className="compInicioSesion">
+      <div className="titulo-responsive">¿Cómo deseas registrarte?</div>
       <div className="left-column">
-        <div id="client" className="boxes">
-          <p className="box-text" id="left-text">
-            Registrarse como <br /> cliente
-          </p>
-          <div className="image"></div>
-          <div className="button" onClick={handleFalse}>
-            <p className="start" id="left-button">
-              Empezar como
-              <br />
-              cliente
+        <div className="responsive-box" onClick={handleFalse}>
+          <div id="client" className="boxes">
+            <p className="box-text" id="left-text">
+              Registrarse como <br /> cliente
             </p>
+            <div className="image"></div>
+            <div className="button" onClick={handleFalse}>
+              <p className="start" id="left-button">
+                Empezar como
+                <br />
+                cliente
+              </p>
+            </div>
           </div>
         </div>
       </div>
       <div className="right-column">
-        <div id="specialist" className="boxes">
-          <p className="box-text" id="right-text">
-            Registrarse como <br /> especialista
-          </p>
-          <div className="image-1"></div>
-          <div className="button" id="right-button-box" onClick={handleTrue}>
-            <p className="start" id="right-button">
-              Empezar como
-              <br />
-              especialista
+        <div className="responsive-box" onClick={handleTrue}>
+          <div id="specialist" className="boxes">
+            <p className="box-text" id="right-text">
+              Registrarse como <br /> especialista
             </p>
+            <div className="image-1"></div>
+            <div className="button" id="right-button-box" onClick={handleTrue}>
+              <p className="start" id="right-button">
+                Empezar como
+                <br />
+                especialista
+              </p>
+            </div>
           </div>
         </div>
       </div>
